b2daa95d5f3b6bd7d852e8cffc5823ba117fc412
kenett <kenettandresdiaztaype@gmail.com>
2021-10-28 15:48:15 -0500

    Creacion del proyecto

---
+++ src/main/java/com/example/demo/PryectoDawiApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PryectoDawiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PryectoDawiApplication.class, args);
+		System.out.println("PRoyecto iniciado :'3 ...");
+	}
+
+}
